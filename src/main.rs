62c92b9a542cec3150f2f676e342c052152fccc1
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-06 21:49:30 -0800

    Day 7

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
+
 fn main() {
     //day1::day1(); // Day 1
     //day2::day2(); // Day 2
     //day3::day3() // Day 3
     //day4::day4() // Day 4
     //day5::day5() // Day 5
-    day6::day6() // Day 6
+    //day6::day6() // Day 6
+    day7::day7() // Day 7
 }
