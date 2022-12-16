5a4a761b6b60c6a910d7288e8ebc9cf27c9f792d
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-16 13:06:55 -0800

    Fix some bugs with repl

--- src/day16.rs
+++ src/day16.rs
@@ -8,7 +8,7 @@ struct Valve {
 fn flowrate(valves: &Vec<Valve>, open: &Vec<usize>) -> usize {
   let mut out = 0;
   for val in open {
-    out += val.flow;
+    out += valves[*val].flow;
   }
   out
 }
@@ -18,32 +18,34 @@ fn dfs(valves: &Vec<Valve>, path: &mut Vec<usize>, open: &mut Vec<usize>, timere
     return released;
   }
 
-  let curr = valves[path[path.len()-1]];
+  let curr = &valves[path[path.len()-1]];
   let mut flow = flowrate(valves, open);
   let mut max = 0;
   let mut rel = released;
   let mut timeleft = timerem;
-  for v in curr.next() {
+  for v in curr.next.iter() {
     // TODO: Make sure not going in loops
     rel = released;
 
     
     // Try opening
-    let val = valves[v];
-    if !open.contains(v) {
-      open.push(v);
+    let val = &valves[*v];
+    if !open.contains(&v) {
+      open.push(*v);
       rel += flow;
       flow += val.flow;
       timeleft -= 1;
     }
 
     // Go there
-    path.push(v);
+    path.push(*v);
     rel += flow;
+    timeleft -= 1;
     let res = dfs(valves, path, open, timeleft, rel);
     if res > max {
       max = res;
     }
+    path.pop();
 
     // Go there without opening
     open.pop();
@@ -61,30 +63,30 @@ fn dfs(valves: &Vec<Valve>, path: &mut Vec<usize>, open: &mut Vec<usize>, timere
 
 pub fn day16() {
   // Parse
-  let inp = include_str!("day16.txt");
-  let names = HashSet::new();
-  let nextraw = Vec::new();
-  let valves = Vec::new()
+  let inp = include_str!("day16.txt").replace("tunnel leads to valve", "tunnels lead to valves");
+  let mut names = HashMap::new();
+  let mut nextraw = Vec::new();
+  let mut valves = Vec::new();
   for line in inp.split("\n") {
     let parts = line.split(" has flow rate=").collect::<Vec<_>>();
-    let right = parts[1].split("; tunnels lead to valves ");
+    let right = parts[1].split("; tunnels lead to valves ").collect::<Vec<_>>();
     valves.push(Valve {
       flow: right[0].parse::<usize>().unwrap(),
-      next: Vec::new();
+      next: Vec::new(),
     });
     names.insert(parts[0].replace("Valve ", ""), nextraw.len());
-    nextraw.push(right[1].split("\n").collect());
+    nextraw.push(right[1].split(", ").collect::<Vec<_>>());
   }
 
   // Update indices
   for (i, val) in nextraw.iter().enumerate() {
     for v in val {
-      valves[i].push(names.get(v).unwrap());
+      valves[i].next.push(*names.get(*v).unwrap());
     }
   }
 
   // DFS
-  let mut path = vec![names.get("AA")];
+  let mut path = vec![*names.get("AA").unwrap()];
   let mut open = Vec::new();
   let res = dfs(&valves, &mut path, &mut open, 30, 0);
   println!("Part 1: {}", res);
