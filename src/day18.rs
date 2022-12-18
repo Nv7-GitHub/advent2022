9b17e5b2f8a0135ef50b799afef6a50fab23fd89
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-17 22:22:58 -0800

    Day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -2,35 +2,29 @@ const XLEN: usize = 20;
 const YLEN: usize = 20;
 const ZLEN: usize = 20;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Pos(i32, i32, i32); // x, y, z
-
-// For floodfill algorithm
-#[derive(Debug)]
-struct Voxel {
-  pos: Pos,
-  outside: bool,
-}
 
 struct Board {
   board: Vec<Vec<Vec<bool>>>,
 }
 
 impl Board { 
-  fn flood(&self, pos: Pos, res: &mut Vec<Voxel>, offsets: &Vec<Pos>) {
+  // Floodfills areas where there are not voxels
+  fn flood(&self, pos: Pos, res: &mut Vec<Pos>, offsets: &Vec<Pos>) {
     // Check if pos is valid
     if pos.0 < 0 || pos.1 < 0 || pos.2 < 0 || pos.0 >= self.board.len() as i32 || pos.1 >= self.board[0].len() as i32 || pos.2 >= self.board[0][0].len() as i32 { // Out of bounds
       return;
     }
-    if self.board[pos.0 as usize][pos.1 as usize][pos.2 as usize] == false { // Not a voxel
+    if self.board[pos.0 as usize][pos.1 as usize][pos.2 as usize] == true { // Is a voxel, don't include
       return;
     }
-    if res.iter().map(|x| x.pos).any(|x|  x == pos) { // Already visited
+    if res.contains(&pos) { // Already visited
       return;
     }
 
     // Floodfill
-    res.push(Voxel { pos, outside: false });
+    res.push(pos);
     for v in offsets {
       let newpos = Pos(pos.0 + v.0, pos.1 + v.1, pos.2 + v.2);
       self.flood(newpos, res, offsets);
@@ -45,6 +39,22 @@ impl Board {
         continue;
       }
       if self.board[newpos.0 as usize][newpos.1 as usize][newpos.2 as usize] == false { // Not a voxel
+        continue;
+      }
+      surfarea -= 1;
+    }
+    surfarea
+  }
+
+  // Surfarea but it only counts blank voxels if they are in the domain
+  fn surfarea_with_domain(&self, pos: Pos, offsets: &Vec<Pos>, domain: &Vec<Pos>) -> usize {
+    let mut surfarea = 6;
+    for v in offsets {
+      let newpos = Pos(pos.0 + v.0, pos.1 + v.1, pos.2 + v.2);
+      if newpos.0 < 0 || newpos.1 < 0 || newpos.2 < 0 || newpos.0 >= self.board.len() as i32 || newpos.1 >= self.board[0].len() as i32 || newpos.2 >= self.board[0][0].len() as i32 { // Out of bounds
+        continue;
+      }
+      if self.board[newpos.0 as usize][newpos.1 as usize][newpos.2 as usize] == false && domain.contains(&newpos) { // Not a voxel
         continue;
       }
       surfarea -= 1;
@@ -76,21 +86,35 @@ pub fn day18() {
     board.board[parts[0]][parts[1]][parts[2]] = true;
   }
 
-  // Part 1
-  let mut res = 0;
+  // Part 2
+  // Find all space that is not voxels by floodfilling from bottom right corner (assumes nothing encloses that)
+  let mut blankspace = Vec::new();
+  board.flood(Pos(XLEN as i32-1, YLEN as i32-1, ZLEN as i32-1), &mut blankspace, &offsets);
+  println!("[DEBUG] Space count: {}", blankspace.len());
+
+  // Go through voxels
+  let mut p1 = 0;
+  let mut p2 = 0;
   for x in 0..XLEN {
     for y in 0..YLEN {
       for z in 0..ZLEN {
         if board.board[x][y][z] {
-          res += board.surfarea(Pos(x as i32, y as i32, z as i32), &offsets);
+          let pos = Pos(x as i32, y as i32, z as i32);
+
+          // Part 1
+          p1 += board.surfarea(pos, &offsets);
+
+          // Part 2
+          p2 += board.surfarea_with_domain(pos, &offsets, &blankspace);
         }
       }
     }
   }
-  println!("Part 1: {}", res);
+  println!("Part 1: {}", p1);
+  println!("Part 2: {}", p2);
 
   // Find chunks
-  let mut res = Vec::new();
+  /*let mut res = Vec::new();
   loop {
     // Find voxel
     let mut found = false;
@@ -100,7 +124,7 @@ pub fn day18() {
           if board.board[x][y][z] {
             // Found a voxel, floodfill
             let mut vals = Vec::new();
-            board.flood(Pos(x as i32, y as i32, z as i32), &mut vals, &offsets);
+            board.flood(Pos(x as i32, y as i32, z as i32), &mut vals, &offsets, false);
 
             // Remove from board
             for v in vals.iter() {
@@ -118,5 +142,5 @@ pub fn day18() {
       break
     }
   }
-  println!("Chunk count: {}", res.len());
+  println!("Chunk count: {}", res.len());*/
 }
